7b4e78a5b059d6d59ebfc89681ec3b38a7409bfd
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-04-17 18:55:09 -0600

    need to fix 'model' error

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            
+        }
+
+        public int ProductID { get; set; } 
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string CategoryID { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
