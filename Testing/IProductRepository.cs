eec15c0d2a233cd9b65c109eab523d3ede84cd63
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-05-06 18:42:28 +1200

    corrected ProductReo

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -12,7 +12,5 @@ namespace Testing
         public IEnumerable<Category> GetCategories();
         public Product AssignCategory();
         public void DeleteProduct(Product product);
-
-
     }
 }
--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -20,13 +20,7 @@ namespace Testing
             var product = new Product();
             product.Categories = categoryList;
             return product;
-        }
 
-        public void DeleteProduct(Product product)
-        {
-            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new { id = product.ProductID });
-            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;", new { id = product.ProductID });
-            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
         }
 
         public IEnumerable<Product> GetAllProducts()
@@ -41,21 +35,25 @@ namespace Testing
 
         public Product GetProduct(int id)
         {
-            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", 
-                new { id = id });
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", new { id = id });
         }
 
         public void InsertProduct(Product productToInsert)
         {
             _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
                  new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
-
         }
 
         public void UpdateProduct(Product product)
         {
             _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
              new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;", new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;", new { id = product.ProductID });
         }
     }
 }
